8a6ee00feb3d4df48780fa6a6500b931c12ec727
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-01-12 15:27:03 +0700

    try: profile resposive checkout history

--- src/features/USER/profile/component/Profile-Transaction.tsx
+++ src/features/USER/profile/component/Profile-Transaction.tsx
@@ -14,8 +14,8 @@ export default function ProfileTransaction({ transaction }: ProfileTransactionTy
       <HStack gap={"15px"} w={"full"} display={"flex"} flexDirection={{ base: "column", md: "row" }}>
         <Image
           src={transaction?.product?.images[0]?.imageUrl ?? noImage}
-          width={{ base: "100%", md: "250px", lg: "250px" }}
-          height={{ base: "70vw", md: "250px", lg: "250px" }}
+          width={{ base: "100%", md: "250px", lg: "200px" }}
+          height={{ base: "70vw", md: "250px", lg: "200px" }}
         ></Image>
         <VStack w={"full"}>
           <Box w={"full"}>
