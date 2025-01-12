73e6253a3d60e907258c2b35fed20404a03f2f9d
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-01-12 15:43:30 +0700

    try: fix codingan

--- src/features/USER/profile/component/Profile-Transaction.tsx
+++ src/features/USER/profile/component/Profile-Transaction.tsx
@@ -12,7 +12,9 @@ export default function ProfileTransaction({ transaction }: ProfileTransactionTy
   return (
     <HStack width={"100%"} display={"flex"} justify={"space-between"} bg={"brand.bgYoung"} p={"10px 10px 10px 10px"} color={"white"} rounded={"10px"} alignItems={"center"}>
       <HStack gap={"15px"} w={"full"} display={"flex"} flexDirection={{ base: "column", md: "row" }}>
-        <Image src={transaction?.product?.images[0]?.imageUrl ?? noImage} width={{ base: "100%", md: "50%", lg: "50%" }} height={{ base: "70vw", md: "35vw", lg: "20vw" }}></Image>
+        <Box width={{ base: "100%", md: "50%", lg: "70%" }} height={{ base: "70vw", md: "35vw", lg: "15vw" }}>
+          <Image src={transaction?.product?.images[0]?.imageUrl ?? noImage} width={"100%"} height={"100%"}></Image>
+        </Box>
         <VStack w={"full"}>
           <Box w={"full"}>
             <Text color={"brand.active"} textTransform={"capitalize"}>
